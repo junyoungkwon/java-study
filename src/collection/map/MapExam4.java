6e2747b9665ba0a24ba9ebf0fdb68c59242d045a
junyoungkwon <junyoungkwon@hotmail.com>
2019-03-06 17:49:30 +0900

    diagram, MapExam

---
+++ src/collection/map/MapExam4.java
@@ -0,0 +1,30 @@
+package collection.map;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MapExam4 {
+
+	public static void main(String[] args) {
+		Map<String,String> map = new HashMap<>();
+		map.put("이름", "홍길동");
+		map.put("나이", "33");
+		map.put("주소", "서울시 강서구");
+		Map<String,String> map1 = new HashMap<>();
+		map1.put("이름", "김길동");
+		map1.put("나이", "2");
+		map1.put("주소", "인천 계양구");
+		Map<String,String> map2 = new HashMap<>();
+		map2.put("이름", "고길동");
+		map2.put("나이", "44");
+		map2.put("주소", "부산 강서구");
+		List<Map<String,String>> mapList = new ArrayList<>();
+		mapList.add(map);
+		mapList.add(map1);
+		mapList.add(map2);
+		System.out.println(mapList.get(0).get("이름"));
+	}
+	
+}
---
+++ src/collection/map/MapExam5.java
@@ -0,0 +1,33 @@
+package collection.map;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+public class MapExam5 {
+
+	public static void main(String[] args) {
+		List<String> intList = new ArrayList<>();
+		intList.add("1");
+		intList.add("2");
+		intList.add("3");
+		for (int i = 0; i < intList.size(); i++) {
+			System.out.println(intList.get(i));
+		}
+		Map<String, String> map = new HashMap<>();
+		map.put("이름", "홍길동");
+		map.put("나이", "22");
+		map.put("주소", "서울");
+		map.put("특기", "술푸기");
+
+		Iterator<String> it = map.keySet().iterator();
+
+		while (it.hasNext()) {
+			String key = it.next();
+			System.out.println("key : " + key);
+			System.out.println("value : " + map.get(key));
+		}
+	}
+}
